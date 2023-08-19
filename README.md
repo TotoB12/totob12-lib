246a55e4565387de7304d9ea05c2079663f55cc5
TotoB12 <totobeliard@outlook.com>
2023-08-19 14:50:20 -0700

    s

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # totob12-lib
 
 i've got no clue what i'm doing
+
+A small demo library for a Medium publication about publishing libraries.
+
+### Installation
+```
+pip install totob12
+```
+
+### Get started
+How to multiply one number by another with this lib:
+
+```Python
+from totob12 import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+result = multiplication.multiply(5)
+```
---
+++ setup.py
@@ -0,0 +1,42 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="totob12",
+    version="0.1.0",
+    description="TotoB12 library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://totob12.readthedocs.io/",
+    author="Antonin",
+    author_email="antonin@beliard.io",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent"
+    ],
+    packages=["totob12"],
+    include_package_data=True,
+    install_requires=["numpy"]
+)
