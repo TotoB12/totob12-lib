ea78145418179287b26f9f66fbf61d2435e36c45
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-19 17:23:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-def multiply(number, muiltiplier):
+class Multiplication:
     """
-    Multiply a given number by a given multiplier.
+    Instantiate a multiplication operation.
+    Numbers will be multiplied by the given multiplier.
     
-    :param number: The number to multiply.
-    :type number: int
-    
-    :param muiltiplier: The multiplier.
-    :type muiltiplier: int
+    :param multiplier: The multiplier.
+    :type multiplier: int
     """
     
-    return number * muiltiplier
+    def __init__(self, multiplier):
+        self.multiplier = multiplier
+    
+    def multiply(self, number):
+        """
+        Multiply a given number by the multiplier.
+        
+        :param number: The number to multiply.
+        :type number: int
+    
+        :return: The result of the multiplication.
+        :rtype: int
+        """
+        
+        return number * self.multiplier
 
-# Call the function
-print(multiply(5, 2))
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
