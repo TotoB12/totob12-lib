a2033926e58ee374a77b33966015c29984a2f17b
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-19 17:20:22 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def multiply(number, muiltiplier):
+    """
+    Multiply a given number by a given multiplier.
+    
+    :param number: The number to multiply.
+    :type number: int
+    
+    :param muiltiplier: The multiplier.
+    :type muiltiplier: int
+    """
+    
+    return number * muiltiplier
+
+# Call the function
+print(multiply(5, 2))
