8dd8635ff5c5684956654a0e1a690f035418f38e
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-19 20:46:21 -0400

    Create weather.py

---
+++ totob12/weather.py
@@ -0,0 +1,17 @@
+import requests
+
+class weather:
+    def get_quick_weather(unit):
+        """
+        Get the weather.
+
+        :param unit: The unit of the weather.
+        :type unit: str
+
+        :return: The weather.
+        :rtype: str
+        """
+        url = f"https://wttr.in/?format=3&{unit if unit else 'm'}"
+        response = requests.get(url)
+        return response.text
+    
