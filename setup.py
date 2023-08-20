048f1f3b8f04f41bd15e76c72978d2652b5795dd
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-19 20:47:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="totob12",
-    version="0.2.0",
+    version="0.2.1",
     description="TotoB12 library",
     long_description=long_description,
     long_description_content_type="text/markdown",
